6d77fc5551e93ae5e9983d6c69ab115b4011d171
Haejin Jo <classhjo@lnxsrv06.seas.ucla.edu>
2017-04-23 22:43:36 -0700

    Clarified further test of fix in lab report

--- ass3/comm.py
+++ ass3/comm.py
@@ -1,1 +1,15 @@
-#!/bin/python
+#!/usr/bin/python
+
+import random, sys
+from optparse import OptionParser 
+
+class comm: 
+	def __init__(self, sys.argv[1]): 
+		f1 = open (sys.argv[1], 'r') 
+		self.lines = f1.readlines() 
+		f1.close () 
+	def __init__(self, sys.argv[2]): 
+		f2 = open (sys.argv[2], 'r')
+		self.lines = f2.readlines()
+		f2.close()
+
