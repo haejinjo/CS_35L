c1f7d452b59eb66281651c5da6185fd16fc3f924
Haejin Jo <classhjo@lnxsrv06.seas.ucla.edu>
2017-05-05 21:43:42 -0700

    tr2b and tr2u works

---
+++ ass5/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc tr2u.c -o tr2u
--- ass5/tr2u.c
+++ ass5/tr2u.c
@@ -1,8 +1,58 @@
 //UNBUFFERED I/O
-#include <unistd> //for read write with nbytes set to 1 (for 1 char at a time)
+#include <unistd.h> //for read write with nbytes set to 1 (for 1 char at a time)
+#include <stdbool.h> 
+#include <stdio.h>
 
-int main (int argc, char** argv) {
 
+int main (int argc, char** argv) 
+{
+	
+	/* error check for number of args user gives us (argv[0] is the command name itself) */ 
+  if (argc != 3)
+  {
+  	const char arg_msg[] = "Error -- tr2b needs 2 arguments\n";
+  	 write(2, arg_msg, sizeof(arg_msg)-1);
+
+    return -1;
+  }
+
+   /* Store the src and dst strings...note: argv[1] holds first argument, skipping program name */
+  char* src = argv[1];
+  char* dst = argv[2]; 
+
+  char curr_char[1];
+  bool curr_char_printed = false;
+  char output_char;
+  char* i; 
+  char* j;
+  
+
+	while((read(0, curr_char, 1) != 0))
+	{
+		j = dst;
+		curr_char_printed = false;
+
+		for (i=src; *i != '\0'; i++)
+		{
+			//printf("seen by Vivian Sun\n");
+			if (*curr_char == *i)
+			{
+				output_char = *j;
+
+				write(1, &output_char, 1);
+				curr_char_printed = true;
+				break;
+			}
+			j++;
+
+		}
+
+		if(!curr_char_printed)
+		{
+			write(1, &curr_char, 1);
+		}
+
+	}
 
   return 0;
 }
